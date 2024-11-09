f36a15809ff8df3630f0466101994ccb7b84a0db
shooterdev <vincentbleach@gmail.com>
2024-11-09 21:44:14 +0100

    update size btn menu

--- main_lumo.py
+++ main_lumo.py
@@ -1,7 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QSlider, QLabel
-import brightness
-
+import screen_brightness_control as sbc
 
 class BrightnessControlApp(QWidget):
     def __init__(self):
@@ -13,15 +12,22 @@ class BrightnessControlApp(QWidget):
         # Initialisation de l'interface
         self.layout = QVBoxLayout()
 
+        # Essayer de récupérer la luminosité actuelle
+        try:
+            current_brightness = sbc.get_brightness()  # Luminosité en pourcentage
+        except Exception as e:
+            current_brightness = 50  # Valeur par défaut si l'appel échoue
+            print(f"Erreur lors de la récupération de la luminosité : {e}")
+
         # Label pour afficher la luminosité actuelle
-        self.label = QLabel(f'Luminosité : {brightness.get_brightness() * 100:.0f}%', self)
+        self.label = QLabel(f'Luminosité : {current_brightness}%', self)
         self.layout.addWidget(self.label)
 
         # Slider pour ajuster la luminosité
         self.slider = QSlider()
         self.slider.setOrientation(1)  # Orientation verticale
         self.slider.setRange(0, 100)  # Plage de 0 à 100%
-        self.slider.setValue(brightness.get_brightness() * 100)  # Valeur initiale de la luminosité
+        self.slider.setValue(current_brightness)  # Valeur initiale de la luminosité
         self.slider.valueChanged.connect(self.update_brightness)
         self.layout.addWidget(self.slider)
 
@@ -30,9 +36,8 @@ class BrightnessControlApp(QWidget):
 
     def update_brightness(self):
         # Mise à jour de la luminosité
-        brightness.set_brightness(self.slider.value() / 100)
+        sbc.set_brightness(self.slider.value())  # Mettre la luminosité à la valeur du slider
         self.label.setText(f'Luminosité : {self.slider.value()}%')
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
