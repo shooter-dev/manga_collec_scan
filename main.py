292cf80d0c75799e7459b92caed3c62f410e4054
shooterdev <vincentbleach@gmail.com>
2024-11-09 18:45:32 +0100

    fix main

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication
+
+from app.lib.window.main_windows import MainWindow
+
+
+def main():
+
+    app = QApplication(sys.argv)
+    app.setAttribute(Qt.AA_DisableHighDpiScaling, True)  # Désactiver la mise à l'échelle DPI
+    app.setAttribute(Qt.AA_Use96Dpi, True)  # Forcer un DPI standard
+
+    app.setDesktopFileName("MangaCollecScan")
+    size = recupere_dimension()
+    window = MainWindow(size)
+    window.show()
+    sys.exit(app.exec_())
+
+
+def recupere_dimension() -> QSize:
+    tab_dimention = sys.argv[1].split('x')
+    win_width = int(tab_dimention[0])
+    win_height = int(tab_dimention[1])
+    return QSize(win_width, win_height)
+
+
+if __name__ == '__main__':
+    main()
