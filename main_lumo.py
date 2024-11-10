63309b42c10a2f2fadd8d37b8778ee3218bf41fa
shooterdev <vincentbleach@gmail.com>
2024-11-10 20:07:46 +0100

    update app

--- main_lumo.py
+++ main_lumo.py
@@ -1,12 +1,23 @@
 import psutil
-import time
 
-def get_cpu_percentage():
-    while True:
-        # Obtenir le pourcentage d'utilisation du CPU
-        cpu_percent = psutil.cpu_percent(interval=1)  # interval=1 signifie que l'on mesure l'usage du CPU toutes les 1 seconde
-        print(f"CPU Usage: {cpu_percent}%")
-        time.sleep(1)
+# CPU
+print(f"CPU Usage: {psutil.cpu_percent(interval=1)}%")
+print(f"CPU Count (logical): {psutil.cpu_count(logical=True)}")
+print(f"CPU Frequency: {psutil.cpu_freq().current} MHz")
 
-if __name__ == "__main__":
-    get_cpu_percentage()
+# Mémoire
+virtual_memory = psutil.virtual_memory()
+print(f"Total Memory: {virtual_memory.total / (1024 * 1024):.2f} MB")
+print(f"Used Memory: {virtual_memory.used / (1024 * 1024):.2f} MB")
+print(f"Free Memory: {virtual_memory.free / (1024 * 1024):.2f} MB")
+
+# Disque
+disk_usage = psutil.disk_usage('/')
+print(f"Total Disk Space: {disk_usage.total / (1024 * 1024 * 1024):.2f} GB")
+print(f"Used Disk Space: {disk_usage.used / (1024 * 1024 * 1024):.2f} GB")
+print(f"Free Disk Space: {disk_usage.free / (1024 * 1024 * 1024):.2f} GB")
+
+# Réseau
+net_io = psutil.net_io_counters()
+print(f"Bytes Sent: {net_io.bytes_sent / (1024 * 1024):.2f} MB")
+print(f"Bytes Received: {net_io.bytes_recv / (1024 * 1024):.2f} MB")
