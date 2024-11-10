b92ea4e9c414e77d4dc5391fd0a6e371a5fbd014
shooterdev <vincentbleach@gmail.com>
2024-11-10 20:04:50 +0100

    update app

--- main_lumo.py
+++ main_lumo.py
@@ -1,36 +1,12 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
-from PyQt5.QtGui import QIcon, QPalette, QColor
-from PyQt5.QtCore import Qt
+import psutil
+import time
 
-# Initialiser QApplication avec les arguments de ligne de commande
-app = QApplication(sys.argv)
+def get_cpu_percentage():
+    while True:
+        # Obtenir le pourcentage d'utilisation du CPU
+        cpu_percent = psutil.cpu_percent(interval=1)  # interval=1 signifie que l'on mesure l'usage du CPU toutes les 1 seconde
+        print(f"CPU Usage: {cpu_percent}%")
+        time.sleep(1)
 
-# Paramètres généraux de l'application
-app.setApplicationName("Exemple PyQt5")
-app.setApplicationVersion("1.0.0")
-app.setOrganizationName("MonOrganisation")
-app.setWindowIcon(QIcon("icon.png"))
-
-# Configuration du style et de la palette
-app.setStyle("Fusion")
-palette = QPalette()
-palette.setColor(QPalette.Window, QColor("lightgrey"))
-app.setPalette(palette)
-
-# Configuration de la fenêtre principale
-window = QWidget()
-window.setWindowTitle("Fenêtre Principale")
-window.setGeometry(100, 100, 300, 200)
-
-# Ajouter un label
-layout = QVBoxLayout()
-label = QLabel("Bienvenue dans mon application PyQt5 !")
-layout.addWidget(label)
-window.setLayout(layout)
-
-# Afficher la fenêtre
-window.show()
-
-# Lancer la boucle d'événements de l'application
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    get_cpu_percentage()
