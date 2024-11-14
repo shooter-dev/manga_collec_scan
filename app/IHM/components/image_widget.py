d06b4567ef84d467e82de59d10ee3eeb4640d130
shooterdev <vincentbleach@gmail.com>
2024-11-14 08:06:12 +0100

    update

--- app/IHM/components/image_widget.py
+++ app/IHM/components/image_widget.py
@@ -5,9 +5,16 @@ from app.IHM.utils.image_download_thread import ImageDownloadThread
 
 
 class ImageWidget(QLabel):
-    def __init__(self, parent: QWidget):
+    def __init__(self, parent: QWidget, loading_image_path=None):
         super().__init__(parent)
         self.pixmap = QPixmap()
+
+        # Si une image de chargement est fournie, l'afficher par défaut
+        if loading_image_path:
+            self.loading_pixmap = QPixmap(loading_image_path)
+            self.setPixmap(self.loading_pixmap)  # Affiche l'image de chargement
+        else:
+            self.setText("Chargement...")  # Affiche un texte "chargement" si aucune image de chargement n'est fournie
 
     def load_url(self, url):
         """Télécharge et affiche l'image à partir de l'URL"""
